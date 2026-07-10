e928ee3f932ec4535b345844b67e9bb6e077349e
Suraj Das <surajdas3108@gmail.com>
2026-07-10 15:40:32 +0530

    Fixed environment var issue

--- FrontEnd/src/config.js
+++ FrontEnd/src/config.js
@@ -1,5 +1,5 @@
 export const config = {
-  API_BASE_URL: import.meta.env.API_BASE_URL ?? 'http://localhost:7000',
-  ENV: import.meta.env.ENV ?? 'development',
-  DEBUG: import.meta.env.DEBUG === 'true',
+  API_BASE_URL: import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:7000',
+  ENV: import.meta.env.VITE_ENV ?? 'development',
+  DEBUG: import.meta.env.VITE_DEBUG === 'true',
 };
